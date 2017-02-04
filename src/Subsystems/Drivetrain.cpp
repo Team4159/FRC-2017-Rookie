b6e23bef866016c5242fd3c4b8db0a3c032fa4a6
teamlyle <matthewcho@proofschool.org>
2017-02-03 17:48:49 -0800

    Add files via upload

---
+++ src/Subsystems/Drivetrain.cpp
@@ -0,0 +1,22 @@
+#include "Drivetrain.h"
+#include <Joystick.h>
+#include <VictorSP.h>
+#include "../RobotMap.h"
+
+Drivetrain::Drivetrain() :
+	frc::Subsystem("Drivetrain") {
+	leftMotor = new VictorSP(leftDriveMotor);
+	rightMotor = new VictorSP(rightDriveMotor);
+}
+
+void Drivetrain::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+void Drivetrain::set(double leftValue, double rightValue){
+	leftMotor->Set(leftValue);
+	rightMotor->Set(rightValue);
+}
---
+++ src/Subsystems/Drivetrain.h
@@ -0,0 +1,21 @@
+#ifndef DRIVETRAIN_H
+#define DRIVETRAIN_H
+
+#include <Commands/Subsystem.h>
+#include <Joystick.h>
+#include <VictorSP.h>
+class Drivetrain: public Subsystem {
+public:
+	Drivetrain();
+	void InitDefaultCommand() override;
+	void set(double leftValue, double rightValue);
+private:
+	VictorSP *leftMotor;
+	VictorSP *rightMotor;
+    
+
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+};
+
+#endif  // DRIVETRAIN_H
